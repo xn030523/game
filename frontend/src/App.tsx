b93d84758c4776bb1393553e284c4bae5a620a90
XN030523 <b19813889852@163.com>
2025-12-30 11:28:42 +0800

    8

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-
-
-
+import Game from './components/Game'
 
 function App() {
   return (
+    <div>
+      <h1 style={{ textAlign: 'center', color: '#fff', marginTop: 20 }}>农场游戏</h1>
+      <Game />
+    </div>
   )
 }
 
